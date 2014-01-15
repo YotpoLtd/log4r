f7766f98f67747db8b0ac593164ee4890e0d6639
Max Gulyaev <maxim@yotpo.com>
2014-01-15 11:41:25 +0200

    added more info about log

--- lib/log4r/formatter/jsonformatter.rb
+++ lib/log4r/formatter/jsonformatter.rb
@@ -4,32 +4,26 @@ module Log4r
 
   class JSONFormatter < BasicFormatter
 
+    def initialize(hash = {})
+      super(hash)
+      @instance = (hash['instance'] || hash[:instance] || nil)
+    end
+
     def format(logevent)
       logger = logevent.fullname.gsub('::', '.')
       timestamp = (Time.now.to_f * 1000).to_i
       level = LNAMES[logevent.level]
       message = format_object(logevent.data)
-      exception = message if logevent.data.kind_of? Exception
       file, line, method = parse_caller(logevent.tracer[0]) if logevent.tracer
 
       data = {
           logger: logger,
           timestamp: timestamp,
+          instance: @instance,
+          process: Process.pid.to_s,
           level: level,
-          thread: {
-              NDC: NDC.get,
-              message: message,
-              throwable: exception,
-              location_info: {
-                  method: method,
-                  file: file,
-                  line: line
-              }
-          }
+          message: message
       }
-      MDC.get_context.each do |key, value|
-        (data[:thread][:properties] ||= {})[key] = value
-      end
 
       JSON.dump(data)
     end
